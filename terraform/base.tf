a86faf1a169cf5240ba7ae2e14a43a1a44b51693
Kessler, Luca <kesslerluca@gmail.com>
2023-05-26 09:18:22 +0200

    update providers and packages

--- terraform/base.tf
+++ terraform/base.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers { # <1>
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 4.67"
     }
     archive = {
       source = "hashicorp/archive"
-      version = "~> 2.2"
+      version = "~> 2.3"
     }
   }
 }
